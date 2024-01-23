c1748e7da581013ebeeed1e789b42840e322e7de
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2024-01-23 11:40:49 +0800

    fix the camera and player rotation

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,51 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
-[RequireComponent (typeof (GravityBody))]
-public class FirstPersonController : MonoBehaviour {
-	
-	// public vars
-	public float walkSpeed = 6;
-	public LayerMask groundedMask;
-	
-	// System vars
-	Vector3 moveAmount;
-	Vector3 smoothMoveVelocity;
-	Transform cameraTransform;
-	Rigidbody rigidbody;
+[RequireComponent(typeof(GravityBody))]
+public class FirstPersonController : MonoBehaviour
+{
 
-    void Awake() {
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-		cameraTransform = Camera.main.transform;
-		rigidbody = GetComponent<Rigidbody> ();
-	}
-	
-	void Update() {
+    // public vars
+    public float mouseSensitivityX = 5;
+    public float mouseSensitivityY = 1;
+    float verticalLookRotation;
+    Transform cameraTransform;
 
-		// Calculate movement:
-		float inputX = Input.GetAxisRaw("Horizontal");
-		float inputY = Input.GetAxisRaw("Vertical");
-		
-		Vector3 moveDir = new Vector3(inputX,0, inputY).normalized;
-		Vector3 targetMoveAmount = moveDir * walkSpeed;
-		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
+    GravityBody gravityBody;
 
-        Vector3 inputDir = new Vector3(inputX, 0, inputY).normalized;
+    // System vars
+    Rigidbody rigidbody;
 
-		//if(moveDir != Vector3.zero)
-		//{
-		//	Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+    //new input system
+    PlayerInputEditor playerInputEditor;
 
-		//	transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720f * Time.deltaTime);
+    //movement var
+    Vector2 walkAction;
+    Vector2 moveDirection;
+    Vector3 movements;
+    Vector3 moveAmount;
+    Vector3 smoothVelocity;
 
-  //      }
+    public float moveSpeed = 5f;
 
+    private void OnEnable()
+    {
+        if (playerInputEditor == null)
+        {
+            playerInputEditor = new PlayerInputEditor();
+
+            playerInputEditor.playerMovements.walk.performed += i => walkAction = i.ReadValue<Vector2>();
+            playerInputEditor.playerMovements.walk.canceled += i => walkAction = Vector2.zero;
+        }
+        playerInputEditor.Enable();
     }
-	
-	void FixedUpdate() {
-		// Apply movement to rigidbody
-		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
-		rigidbody.MovePosition(rigidbody.position + localMove);
-	}
+
+    private void OnDisable()
+    {
+        if (playerInputEditor != null)
+        {
+            playerInputEditor.playerMovements.walk.performed -= i => walkAction = i.ReadValue<Vector2>();
+            playerInputEditor.playerMovements.walk.canceled -= i => walkAction = Vector2.zero;
+        }
+        playerInputEditor.Disable();
+    }
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        rigidbody = GetComponent<Rigidbody>();
+        cameraTransform = Camera.main.transform;
+
+        gravityBody = GetComponent<GravityBody>();
+    }
+
+    void Update()
+    {
+        handleWalk();
+
+        // Look rotation:
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60, 60);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    public void handleWalk()
+    {
+        moveDirection = walkAction;
+
+        Vector3 moveDir = new Vector3(moveDirection.x, 0, moveDirection.y).normalized;
+
+        Vector3 targetMoveAmount = moveDir * moveSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothVelocity, .15f);
+
+
+
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + localMove);
+        ConstrainToPlanetSurface();
+    }
+
+    void ConstrainToPlanetSurface()
+    {
+        // Get the direction from the planet center to the player
+        Vector3 gravityUp = (transform.position - gravityBody.planet.transform.position).normalized;
+
+        // Get the rotation to align the player with the planet surface
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, gravityUp) * transform.rotation;
+
+        // Apply the rotation smoothly
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50f * Time.deltaTime);
+    }
 }
