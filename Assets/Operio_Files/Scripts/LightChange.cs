3803a26bdf84214703ee9079e0a253e11d13985c
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2024-01-24 06:04:58 +0800

    Fixed UI + Added Light Effect
    Press Space for Light Effect

---
+++ Assets/Operio_Files/Scripts/LightChange.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using DG.Tweening;
+using Unity.VisualScripting;
+
+public class LightChange : MonoBehaviour
+{
+    public GameObject Light1, Light2, Light3;
+    private Color color1,color2, color3;
+
+    private void Start()
+    {
+        color1 = Light1.GetComponent<Light>().color;
+        color2 = Light2.GetComponent<Light>().color;
+        color3 = Light3.GetComponent<Light>().color;
+    }
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            AlarmLight();
+        }
+    }
+    private Color value;
+    public void AlarmLight()
+    {
+        DG.Tweening.Sequence seq = DOTween.Sequence();
+        seq.Append(DOVirtual.Color(color1, Color.red, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color2, Color.red, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color3, Color.red, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+        seq.AppendInterval(0.2f);
+
+        seq.Append(DOVirtual.Color(Color.red,color1, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color2, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color3, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+        seq.AppendInterval(0.15f);
+
+        seq.Append(DOVirtual.Color(color1, Color.red, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color2, Color.red, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color3, Color.red, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+        seq.AppendInterval(0.15f);
+
+        seq.Append(DOVirtual.Color(Color.red, color1, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color2, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color3, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+        seq.AppendInterval(0.1f);
+
+        seq.Append(DOVirtual.Color(color1, Color.red, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color2, Color.red, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(color3, Color.red, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+        seq.AppendInterval(0.1f);
+
+        seq.Append(DOVirtual.Color(Color.red, color1, 1, value =>
+        {
+            Light1.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color2, 1, value =>
+        {
+            Light2.GetComponent<Light>().color = value;
+        }));
+        seq.Join(DOVirtual.Color(Color.red, color3, 1, value =>
+        {
+            Light3.GetComponent<Light>().color = value;
+        }));
+
+    }
+}
