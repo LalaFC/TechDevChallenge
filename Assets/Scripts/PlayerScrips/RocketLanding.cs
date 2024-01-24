1e5b845837ae2e500025d36fccd41c6e5f382121
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2024-01-24 11:22:57 +0800

    Added Intro to Main Transition

--- Assets/Scripts/PlayerScrips/RocketLanding.cs
+++ Assets/Scripts/PlayerScrips/RocketLanding.cs
@@ -1,15 +1,29 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class RocketLanding : MonoBehaviour
 {
     public GameObject Target;
     private float speed;
+
+    public float fadeDuration = 1.0f;
+    private float TimeCount;
+
+
     void Update()
     {
-        speed = 1;
+        TimeCount = Time.time;
+        if (TimeCount < 2)
+        {
+            speed = 1;
+        }
+
+        else speed = 2;
         Target = GameObject.FindGameObjectWithTag("Planet");
         transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
         LookRotation();
@@ -21,5 +35,18 @@ public class RocketLanding : MonoBehaviour
         Vector3 relativePos = Target.transform.position - this.transform.position;
         Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotspeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Planet")
+        {
+            DOTween.ToAlpha(() => Camera.main.backgroundColor, color => Camera.main.backgroundColor = color, 1f, fadeDuration).OnComplete(() =>
+            {
+            SceneManager.LoadScene(1);
+            });
+
+        }
+
     }
 }
